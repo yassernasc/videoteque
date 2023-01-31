9e74f0ececa9fdff1c59eec4d7d4eb1a2f83f06b
Yasser Nascimento <dev@yasser.email>
2023-01-31 16:17:56 -0300

    server: serve local file

--- cmd/index.go
+++ cmd/index.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"github.com/spf13/cobra"
+	"lugosi/judgment"
 	"lugosi/server"
 	"lugosi/storage"
 )
@@ -14,12 +15,22 @@ var root = &cobra.Command{
 		if len(args) == 0 {
 			return errors.New("Requires a movie")
 		}
-		return nil
+		if len(args) > 1 {
+			return errors.New("Too much blood at once")
+		}
+
+		movie := args[0]
+		if !judgment.IsUrl(movie) && !judgment.IsFile(movie) {
+			return errors.New("Invalid movie entry")
+		} else {
+			return nil
+		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		storage.Movie = args[0]
 		server.Init()
 	},
+	SilenceUsage: true,
 }
 
 func Init() {
--- server/movie.go
+++ server/movie.go
@@ -2,12 +2,22 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"lugosi/judgment"
 	"lugosi/storage"
 	"net/http"
 )
 
 func MovieRoutes(e *echo.Echo) {
 	e.GET("/movie", func(c echo.Context) error {
-		return c.Redirect(http.StatusMovedPermanently, storage.Movie)
+		// check if the current movie is a file or a link
+		redirect := "/static"
+		if judgment.IsUrl(storage.Movie) {
+			redirect = storage.Movie
+		}
+
+		return c.Redirect(http.StatusMovedPermanently, redirect)
+
 	})
+
+	e.File("/static", storage.Movie)
 }
